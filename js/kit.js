e1ce1efedc249d20556dfa5f0a21bade2ea06df3
Anes Mulalić <anes.mulalic@hotmail.com>
2018-02-03 01:35:54 +0100

    Transition fix
    - Remove transition FIX

--- js/kit.js
+++ js/kit.js
@@ -1,8 +1,17 @@
-window.addEventListener('keydown', function(e){
+const keys = document.querySelectorAll('.key');
+
+window.addEventListener('keydown', playSound);
+window.addEventListener('keyup', function (e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(key && key.classList.contains('playing')) key.classList.remove('playing');
+  });
+  
+
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`key[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-});
+}
